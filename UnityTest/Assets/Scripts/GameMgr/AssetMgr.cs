73f251e75d380fbb994ae803694d9326e7643eea
silingfei <976983385@qq.com>
2015-01-25 14:54:42 +0800

    add player plugin

---
+++ UnityTest/Assets/Scripts/GameMgr/AssetMgr.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AssetMgr
+{
+    //note:the class is add const path
+    //==========player=============
+    public const string FlowerMonster_Blue = "PlayerPrefabs/FlowerMonster-Blue";
+    public const string FlowerMonster_Brown = "PlayerPrefabs/FlowerMonster-Brown";
+    public const string FlowerMonster_Purple = "PlayerPrefabs/FlowerMonster-Purple";
+    public const string FlowerMonster_Red = "PlayerPrefabs/FlowerMonster-Red";
+    public const string MagmaDemon_Blue = "PlayerPrefabs/MagmaDemon-Blue";
+    public const string MagmaDemon_Green = "PlayerPrefabs/MagmaDemon-Green";
+    public const string MagmaDemon_Purple = "PlayerPrefabs/MagmaDemon-Purple";
+    public const string MagmaDemon_Orange = "PlayerPrefabs/MagmaDemon-Orange";
+}
---
+++ UnityTest/Assets/Scripts/UIControl/PlayerControl.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+    public GameObject playerRoot;
+    public int playerCount;
+    void Awake()
+    {
+        if (playerRoot == null)
+        {
+            playerRoot = GameObject.Find("PlayerRoot");
+            if (playerRoot == null)
+            {
+                throw new UnityException("the hierarchy is not PlayerRoot");
+            }
+        }
+        playerCount = playerCount <= 0 ? 5 : playerCount;
+    }
+
+    void Start()
+    {
+        InstancePlayer();
+    }
+
+
+    private void InstancePlayer()
+    {
+        int xCount = playerCount / 2;
+        int yCount = playerCount % 2 == 0 ? playerCount / 2 : (playerCount - playerCount / 2);
+        for (int i = 0; i < xCount; i++)
+        {
+            for (int j = 0; j < yCount; j++)
+            {
+                GameObject go = Instantiate(GetRandomPlayer()) as GameObject;
+                go.transform.parent = playerRoot.transform;
+                go.transform.localPosition = new Vector3(i * 3, 0, j * 3);
+            }
+        }
+    }
+
+    private GameObject GetRandomPlayer()
+    {
+        string[] paths = { 
+                        AssetMgr.MagmaDemon_Blue,AssetMgr.MagmaDemon_Green,AssetMgr.MagmaDemon_Orange,AssetMgr.MagmaDemon_Purple,
+                        AssetMgr.FlowerMonster_Blue,AssetMgr.FlowerMonster_Brown,AssetMgr.FlowerMonster_Purple,AssetMgr.FlowerMonster_Red
+                         };
+        return Resources.Load(paths[Random.Range(0, paths.Length - 1)]) as GameObject;
+    }
+}
